29762d583a5cc3a0a25b0757f691321f2964f0d7
Totokis <pawelj_janusz@wp.pl>
2024-01-10 16:50:52 +0100

    feat(main): Almost done with detailed screen

--- translations.js
+++ translations.js
@@ -2,27 +2,33 @@ export const translations = {
     'en': {
         'welcome': 'Welcome to my website',
         'about': 'About me',
-        'portfolioTitle': 'Your Portfolio',
+        'portfolioTitle': 'Paweł Janusz portfolio',
         'name': 'John Doe',
-        'position': 'Web Developer',
+        'position1': 'C# Developer',
+        'position2': 'Unity VR Developer',
         'darkMode': 'Dark Mode',
         'projects': 'Projects',
         'hobby-projects': 'Hobby Projects',
+        'about-me': 'About me',
         'project-name1': 'Project Name',
-        'project-desc1': 'Description'
+        'project-desc1': 'Description',
+        'contact': 'Contact'
         // Inne tłumaczenia w języku angielskim
     },
     'pl': {
         'welcome': 'Witaj na mojej stronie',
         'about': 'O mnie',
-        'portfolioTitle': 'Twoje Portfolio',
+        'portfolioTitle': 'Portfolio',
         'name': 'Jan Kowalski',
-        'position': 'Programista Webowy',
+        'position1': 'Programista C#',
+        'position2': 'Unity VR Deweloper',
         'darkMode': 'Tryb Ciemny',
         'projects': 'Projekty',
         'hobby-projects': 'Projekty w wolnym czasie',
+        'about-me': 'Trochę o mnie',
         'project-name1': 'Nazwa projektu',
-        'project-desc1': 'Opis'
+        'project-desc1': 'Opis',
+        'contact': 'Kontakt'
         // Inne tłumaczenia w języku polskim
     }
     // Możesz dodać więcej języków
